28f93175bbc2fd8e21ecc264786f23cb7fd6ba7e
kbryzgin <dev@bryzgin.ru>
2026-09-10 14:41:52 +0700

    feat: add mapping and encoding core interface

--- include/encoding.h
+++ include/encoding.h
@@ -1,3 +1,12 @@
 /*
- * mapping and encoding interface
- * /
+ * encoding.h - mapping and encoding core
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+uint16_t cp1251_to_unicode_mapping(uint8_t byte);
+
+int unicode_to_utf8_encoding(uint16_t code_point, uint8_t* buffer);
+
