0b6ea6d7391113b3a229fd1ac1bdeaa9933fc5c5
kbryzgin <dev@bryzgin.ru>
2026-09-11 10:36:45 +0700

    feat: add convert file interface

--- include/encoding.h
+++ include/encoding.h
@@ -5,8 +5,11 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdio.h>
 
 uint16_t cp1251_to_unicode_mapping(uint8_t byte);
 
 int unicode_to_utf8_encoding(uint16_t code_point, uint8_t* buffer);
+
+int convert_file(FILE *input, FILE *output);
 
